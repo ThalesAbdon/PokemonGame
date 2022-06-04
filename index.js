22020a58124b847d49c1709be6cf962a88fe2150
Thales <thales.sousufpi@gmail.com>
2022-06-04 15:12:21 -0300

    Criando o escopo do projeto

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log("Hello World")
+const canvas = document.querySelector('canvas')
+const k = canvas.getContext('2d')
+
+canvas.width = 1024
+canvas.height = 576
+k.fillStyle = 'white';
+k.fillRect(0,0,canvas.width,canvas.height)
