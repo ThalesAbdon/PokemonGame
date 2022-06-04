8a278e5ecda98c278638c73afcbd2db9f0f62578
Thales <thales.sousufpi@gmail.com>
2022-06-04 16:05:34 -0300

    Criando o personagem na tela do jogo

--- index.js
+++ index.js
@@ -7,9 +7,23 @@ k.fillStyle = 'white';
 k.fillRect(0,0,canvas.width,canvas.height)
 
 const image = new Image()
-image.src = './MAPA/mapTeste.png'
+image.src = './IMG/map.png'
 
+const playerImagem = new Image()
+playerImagem.src = './IMG/playerDown.png' 
 
 image.onload = () => {
     k.drawImage(image,-650,-450)
+    k.drawImage(playerImagem,
+        0,
+        0,
+        playerImagem.width/4,
+        playerImagem.height,
+        canvas.width /2 -  playerImagem.width / 4 , 
+        canvas.height / 2 - playerImagem.height / 2,
+        playerImagem.width / 4,
+        playerImagem.height)
 }
+
+
+
