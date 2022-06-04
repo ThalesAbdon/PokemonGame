9ce1bfabbe45cdecfcf11b6125d0a03ac716cb90
Thales <thales.sousufpi@gmail.com>
2022-06-04 15:24:48 -0300

    Importando nosso cenário do jogo para usarmos como background

--- index.js
+++ index.js
@@ -5,3 +5,11 @@ canvas.width = 1024
 canvas.height = 576
 k.fillStyle = 'white';
 k.fillRect(0,0,canvas.width,canvas.height)
+
+const image = new Image()
+image.src = './MAPA/mapTeste.png'
+
+
+image.onload = () => {
+    k.drawImage(image,-650,-450)
+}
