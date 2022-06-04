9e714a4fb8c4c6572c65e4ebee106ce27f17ea38
Thales <thales.sousufpi@gmail.com>
2022-06-04 17:00:58 -0300

    Adicionado movimento ao jogador!

--- index.js
+++ index.js
@@ -12,8 +12,43 @@ image.src = './IMG/map.png'
 const playerImagem = new Image()
 playerImagem.src = './IMG/playerDown.png' 
 
-image.onload = () => {
-    k.drawImage(image,-650,-450)
+class Sprite {
+    constructor({position,velocity,image}){
+        this.position = position
+        this.image = image
+    }
+
+    draw(){
+        k.drawImage(this.image, this.position.x, this.position.y)
+    }
+}
+
+const background = new Sprite({
+    position: {
+        x: -650,
+        y: -450
+    },
+    image: image
+})
+
+const keys = {
+    w: {
+        pressed: false
+    },
+    a: {
+        pressed: false
+    },
+    s: {
+        pressed: false
+    },
+    d: {
+        pressed: false
+    }
+}
+
+function animate(){
+    window.requestAnimationFrame(animate)
+    background.draw()
     k.drawImage(playerImagem,
         0,
         0,
@@ -23,7 +58,58 @@ image.onload = () => {
         canvas.height / 2 - playerImagem.height / 2,
         playerImagem.width / 4,
         playerImagem.height)
+
+      if(keys.w.pressed && lastKey === 'w') background.position.y += 5
+      else if(keys.a.pressed && lastKey === 'a') background.position.x += 5
+      else if(keys.s.pressed && lastKey === 's') background.position.y -= 5
+      else if(keys.d.pressed && lastKey === 'd') background.position.x -= 5
+      
+      
 }
+animate()
+
+let lastKey = ''
+
+window.addEventListener('keydown' , (e) => {
+    switch(e.key){
+        case 'w':
+           keys.w.pressed = true
+           lastKey = 'w'
+            break
+        case 'a':
+            keys.a.pressed = true
+            lastKey = 'a'
+            break
+        case 's':
+            keys.s.pressed = true
+            lastKey = 's'
+            break
+        case 'd':
+            keys.d.pressed = true
+            lastKey = 'd'
+            break         
+    }
+})
+
+window.addEventListener('keyup' , (e) => {
+    switch(e.key){
+        case 'w':
+           keys.w.pressed = false
+           console.log("nao está sendo pressionado")
+            break
+        case 'a':
+            keys.a.pressed = false
+            break
+        case 's':
+            keys.s.pressed = false
+            break
+        case 'd':
+            keys.d.pressed = false
+            break         
+    }
+})
+
+
 
 
 
